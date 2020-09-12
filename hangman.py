3ea33a8ba923be4ad4668a6c24382359cb7025d8
Mubashir Chandle <61664272+mubashirchandle@users.noreply.github.com>
2020-09-12 23:25:04 +0530

    Add buttons for alphabets

--- hangman.py
+++ hangman.py
@@ -3,6 +3,7 @@ import os
 
 # Colors
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 # Setup display
 pygame.init()
@@ -13,6 +14,18 @@ win = pygame.display.set_mode((WIDTH, HEIGHT))
 # Load images
 images = [pygame.image.load(f'images/hangman{i}.png') for i in range(6)]
 
+# Button variables
+RADIUS = 20
+GAP = 15
+BUTTON_BOX_WIDTH = (RADIUS * 2 * 13) + (GAP * 12)   # Total width that all the (buttons + gaps) take.
+START_X = ((WIDTH - BUTTON_BOX_WIDTH) / 2) + RADIUS # Starting offset for first button
+START_Y = 400
+letters_pos = []
+for i in range(26):
+    x = round(START_X + ((RADIUS * 2 + GAP) * (i % 13)))
+    y = round(START_Y + ((RADIUS * 2 + GAP) * (i // 13)))
+    letters_pos.append((x, y))
+
 # Game variables
 hangman_status = 0
 
@@ -21,13 +34,24 @@ FPS = 60
 clock = pygame.time.Clock()
 run = True
 
+def draw():
+    win.fill(WHITE)
+
+    # Draw alphabet buttons
+    for letter_pos in letters_pos:
+        x, y = letter_pos
+        pygame.draw.circle(win, BLACK, (x, y), RADIUS, 3)
+
+    # Replace existing hangman image with new one.
+    win.blit(images[hangman_status], (150, 100))
+
+    pygame.display.update()
+
+
 while run:
     clock.tick(FPS)
 
-    # Replace existing hangman image with new one.
-    win.fill(WHITE)
-    win.blit(images[hangman_status], (150, 100))
-    pygame.display.update()
+    draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
